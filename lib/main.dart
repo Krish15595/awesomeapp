e76a98bc599309566c31041e64578137c880aef4
Krish15595 <gajulakrishna007@gmail.com>
2021-01-09 19:12:06 +0530

    chap 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          // color: Colors.teal,     if decoration is used then color should be comment
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey, blurRadius: 5, offset: Offset(2, 5))
-              ],
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
-          child: Text(
-            "I am a Box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              // width: MediaQuery.of(context).size.width,  //screen width will be assign
+              // height: MediaQuery.of(context).size.height/2,
+              width: 200,
+              height: 400,
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(        //row and column 2 function there
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
