a327ff057e91da10dddfb82bbcd030280358cb5d
Krish15595 <gajulakrishna007@gmail.com>
2021-01-09 18:37:45 +0530

    decoration properties and container properties

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,31 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("hello world")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          // color: Colors.teal,     if decoration is used then color should be comment
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(2, 5))
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+          child: Text(
+            "I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),
+          ),
+        ),
       ),
     );
   }
